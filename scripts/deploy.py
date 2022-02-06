1eef271d10a0d5cae1f6ccdf368e255f49ab3737
SRVng <snakglom@gmail.com>
2022-02-07 00:05:48 +0700

    add contracts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,26 @@
-from brownie import RSVP_Event, accounts
 import json
+from brownie import RSVP_Event, EVT_Token, Faucet, CreatorERC721, accounts
 
 def main():
-    acct = accounts.load('RSVP.json')
-    RSVP_Event.deploy(10000000, {'from':acct})
+    myAccount = accounts.load('RSVP.json')
+
+    token = EVT_Token.deploy(10000000 * 1e18, {'from': myAccount})
+    faucet = Faucet.deploy({'from': myAccount})
+    NFT = CreatorERC721.deploy({'from': myAccount})
+    instance =  RSVP_Event.deploy(token, NFT, myAccount, {'from':myAccount})
+
+    fetch_abi()
+
+def fetch_abi():
+
+    contractList = [EVT_Token, Faucet, CreatorERC721, RSVP_Event]
+    contractAbiList = ['tokenAbi', 'faucetAbi', 'nftAbi', 'rsvpAbi']
+
+    for i in range(len(contractList)):
+        struct = {
+            "CONTRACT_ADDRESS": str(contractList[i][-1]),
+            "CONTRACT_ABI": contractList[i][-1].abi
+        }
+
+        with open(('./app/src/' + contractAbiList[i] + '.json'), 'w', encoding='utf-8') as f:
+            json.dump(struct, f, indent=3)
